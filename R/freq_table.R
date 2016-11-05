a9b05b284aba703931f2d1894ca536a2adfbb588
Nathan Trenholm <nathant23@gmail.com>
2016-11-05 00:39:56 -0700

    Initial commit. Contians freq_table and freq_table2

---
+++ R/freq_table.R
@@ -0,0 +1,32 @@
+#' freq_table
+#'
+#' freq_table returns a tibble (trimmed down version of data.frame) that is a frequency table with
+#'   counts and percentages of values from the provided data frame and column.
+#'
+#' @param data_frame_string a string of the name of a data frame
+#' @param column_string a string of the column to be aggregated
+#'
+#' @return a tibble containing the counts and percentages of each value from the provided data
+#' @export
+#'
+#' @examples
+#' tbl <- data_frame(numbers = sample(1:10, 200, replace = TRUE),
+#'                   letters = sample(letters, 200, replace = TRUE))
+#' freq_table('tbl', 'numbers')
+#' View(freq_table('tbl', 'letters'))
+freq_table <- function(data_frame_string, column_string) {
+
+  result <- count_(get(data_frame_string), column_string) %>%
+              mutate(total = sum(n)) %>%
+              group_by_(column_string) %>%
+              mutate(Percentage = round(n * 100 / total, 1)) %>%
+              select_(column_string,
+                      Count = 'n',
+                      'Percentage')
+  return(tibble(result))
+ }
+
+
+
+
+
