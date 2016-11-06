ad860faa26009ff0880ee38be9fc842a1e4289fe
Nathan Trenholm <nathant23@gmail.com>
2016-11-06 04:59:42 -0800

    Updated examples to use data.frame instead of data_frame.

--- R/freq_table.R
+++ R/freq_table.R
@@ -3,15 +3,15 @@
 #' freq_table returns a frequency table with counts and percentages of values
 #'   from the provided data frame and column.
 #'
-#' @param data_frame_string a string of the name of a data frame
-#' @param column_string a string of the column to be aggregated
+#' @param data_frame_string   a string of the name of a data frame
+#' @param column_string   a string of the column to be aggregated
 #'
 #' @return a tibble containing the counts and percentages of each value from the provided data
 #' @export
 #' @importFrom dplyr "%>%"
 #'
 #' @examples
-#' tbl <- data_frame(numbers = sample(1:10, 200, replace = TRUE),
+#' tbl <- data.frame(numbers = sample(1:10, 200, replace = TRUE),
 #'                   letters = sample(letters, 200, replace = TRUE))
 #' freq_table('tbl', 'numbers')
 #' View(freq_table('tbl', 'letters'))
